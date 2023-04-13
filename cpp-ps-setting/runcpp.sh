48f2c85cb011e939b395acf780bd5bae013f3c7b
J0onYEong <cjy2134@naver.com>
2023-04-13 14:37:02 +0900

    [Feat] c++ 파일 실행 후 실행파일 삭제기기능 추가 Feat: 실행파일 삭제 runcpp로 cpp코드를 실행한 후 실행파일이 삭제됩니다.

--- cpp-ps-setting/runcpp.sh
+++ cpp-ps-setting/runcpp.sh
@@ -9,7 +9,7 @@ output=$nowPath/output.txt
 
 if [ -e $solution ] && [ -e $input ];
 then
-    g++ -o $execute $solution && $execute < $input > $output
+    g++ -o $execute $solution && $execute < $input > $output && rm $execute
 else
     echo "runcpp: \033[31merror: \033[36mplease initialize your directory first by writing \033[33mpscpp\033[0m"
 fi
